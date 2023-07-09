ecff4d575c80c015d3a0bd0073a10c07d8d7f65a
Peter Eldritch <peter.jirak@gmail.com>
2023-07-08 22:43:05 -0700

    Not using the file src/js/main.js

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const onLoadHandler = () => {
-    const appContainer = document.getElementById('app-container');
-    const clientWidth = appContainer.clientWidth;
-    const clientHeight = appContainer.clientHeight;
-}
