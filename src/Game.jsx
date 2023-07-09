21f3e67ff4a36e19c2aa19c65ae34eca6b0bba4a
Peter Eldritch <peter.jirak@gmail.com>
2023-07-08 17:34:48 -0700

    Starting to ikmplement cards.

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,9 +1,25 @@
+import cardBack from './images/game-images/card-back.png';
+
+const GameCard = (props) => {
+    return (
+        <img className='game-card' src={cardBack}>
+        </img>
+    );
+}
+
 const GameRow = (props) => {
     const size = props.size;
     const rowIndex = props.rowIndex;
+    const cards = [];
+
+    for (let i = 0; i < size; i += 1) {
+        let card = <GameCard />
+        cards.push(card);
+    }
 
     return (
         <div className='game-row'>
+            {cards}
         </div>
     );
 }
