5878502f59e62ed67f06ec8e114a67ae699d29aa
Peter Eldritch <peter.jirak@gmail.com>
2023-07-09 09:18:17 -0700

    Fixed a defect in the dynamicResizeUtilities. I made a mistake. I thought the game board had one child for each row and one child for each spacer and that there was one spacer for each row except the last row. That is incorrect. The game has two children for each row, one for each row and one spacer after each row. The last row also has a spacer. I fixed this defect.

--- src/js/dynamicResizeUtilities.js
+++ src/js/dynamicResizeUtilities.js
@@ -4,31 +4,29 @@ const dynamicallySizeGameElements = () => {
     const gameBoardElement = document.getElementById('game-board');
     if (gameBoardElement) {
         const childCount = gameBoardElement.children.length;
-        if (childCount % 2 !== 1) {
+        if (childCount % 2 !== 0) {
             throw new Error('Unable to determine how many rows the game ' +
                             'board has. The number of child elements of ' +
                             `the game board is ${childCount}. Expect one ` +
                             'child element for each row plus one spacer ' +
-                            'after each row except the last one, that ' +
-                            'number should always be odd, adding one to ' +
-                            'that number and dividing by 2 should give ' +
-                            'the number of rows, however the child count ' +
-                            `is ${childCount} that number is even. Cannot ` +
-                            'determine the number of rows on the game board.');
+                            'after each row, that number should always be even. ' +
+                            'Dividing that number by 2 should give the number ' +
+                            `of rows, however the child count is ${childCount} ` +
+                            'that number is odd. Cannot determine the number ' +
+                            'of rows on the game board.');
         }
-        const size = ( childCount + 1 ) / 2;
+        const size = childCount / 2;
         if (![4, 6, 8, 10, 12].includes(size)) {
             throw new Error('Failed to determine the number of rows in the ' +
                             'game. A game should have either 4, 6, 8, 10, or ' +
                             '12 rows. Attempted to determine the number of ' +
                             'rows this game has by getting the game-board ' +
-                            'element, getting a count of its children, ' +
-                            'adding 1 and then dividing by two. The ' +
-                            'game-board element should have one child for ' +
-                            'every row, place one spacer element for every ' +
-                            'row except the last row. That calculation ' +
-                            `produced the value ${size}. That value is not ` +
-                            '4, 6, 8, 10, nor 12.');
+                            'element, getting a count of its children, and ' +
+                            'then dividing by two. The game-board element ' +
+                            'should have two children for every row -- a ' +
+                            `row element and a spacer element. That ' +
+                            'calculation produced the value ${size}. ` +
+                            'That value is not 4, 6, 8, 10, nor 12.');
         }
         const viewportHeight = window.innerHeight;
         const viewportWidth = window.innerWidth;
