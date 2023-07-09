ed5d174a215be750ef7d2234dd37c6532e9fc2c4
Peter Eldritch <peter.jirak@gmail.com>
2023-07-08 19:41:13 -0700

    Working on positioning and sizing of cards.

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,19 @@
+import { useLayoutEffect } from 'react';
 import cardBack from './images/game-images/card-back.png';
+import { isPending } from '@reduxjs/toolkit';
+
+const rowStyles = {
+    '4': 'four-rows',
+    '6': 'six-rows',
+    '8': 'eight-rows',
+    '10': 'ten-rows',
+    '12': 'twelve-rows'
+}
 
 const GameCard = (props) => {
+    const cardIndex = props.cardIndex;
     return (
-        <img className='game-card' src={cardBack}>
+        <img id={`card-${cardIndex}`} className='game-card' src={cardBack}>
         </img>
     );
 }
@@ -13,12 +24,13 @@ const GameRow = (props) => {
     const cards = [];
 
     for (let i = 0; i < size; i += 1) {
-        let card = <GameCard />
+        let cardIndex = rowIndex * size + i;
+        let card = <GameCard cardIndex={cardIndex}/>
         cards.push(card);
     }
 
     return (
-        <div className='game-row'>
+        <div id={`game-row-${rowIndex}`} className={`game-row ${rowStyles[size]}`}>
             {cards}
         </div>
     );
@@ -37,8 +49,24 @@ const Game = (props) => {
         rows.push(row)
     }
 
+    useLayoutEffect(
+        () => {
+            const gameRowIndex0 = document.getElementById('game-row-0');
+            const rowWidth = gameRowIndex0.offsetWidth;
+            const rowHeight = Math.floor( gameRowIndex0.offsetHeight );
+            const cellWidth = Math.floor( rowWidth / size );
+            const dimension = rowHeight < cellWidth ? rowHeight : cellWidth;
+
+            for (let i = 0; i < size * size; i += 1) {
+                const card = document.getElementById(`card-${i}`);
+                card.style.height = `${dimension}px`;
+                card.style.width = `${dimension}px`;
+            }
+        }
+    );
+
     return (
-        <div className="game-board">
+        <div id='game-board' className="game-board">
             {rows}
         </div>
     );
