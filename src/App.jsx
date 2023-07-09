d988c0c5f2e337f6c3b6c1a809a2fb97af5f4575
Peter Eldritch <peter.jirak@gmail.com>
2023-07-08 21:20:10 -0700

    Dropped outer <> ... </> elements as they were unnecessary in this case.

--- src/App.jsx
+++ src/App.jsx
@@ -16,36 +16,34 @@ function App() {
     const size = parseInt(match[1]);
 
     return (
-        <>
-            <div id="app-container" className="app-container">
-                <div className="above-game">
-                   <div className="above-game-left">
-                       <DropdownSingleItemSelector
-                           items='"Fairies", "Super Heroes", "Humanity in Space"'
-                           selected={collection}
-                           setSelected={setCollection}
-                           isOpen={collectionSelectorOpen}
-                           setToOpen={setCollectionSelectorOpen}
-                       />
-                   </div>
-                   <div className="above-game-middle">
-                       <p className="game-title">The Match Game</p>
-                   </div>
-                   <div className="above-game-right">
-                   <DropdownSingleItemSelector
-                           items='"4 x 4", "6 x 6", "8 x 8", "10 x 10", "12 x 12"'
-                           selected={selectedDimension}
-                           setSelected={setSelectedDimension}
-                           isOpen={dimensionSelectorOpen}
-                           setToOpen={setDimensionSelectorOpen}
-                   />
-                   </div>
+        <div id="app-container" className="app-container">
+            <div className="above-game">
+                <div className="above-game-left">
+                    <DropdownSingleItemSelector
+                        items='"Fairies", "Super Heroes", "Humanity in Space"'
+                        selected={collection}
+                        setSelected={setCollection}
+                        isOpen={collectionSelectorOpen}
+                        setToOpen={setCollectionSelectorOpen}
+                    />
                 </div>
-                <Game
-                    size={size}
+                <div className="above-game-middle">
+                    <p className="game-title">The Match Game</p>
+                </div>
+                <div className="above-game-right">
+                <DropdownSingleItemSelector
+                        items='"4 x 4", "6 x 6", "8 x 8", "10 x 10", "12 x 12"'
+                        selected={selectedDimension}
+                        setSelected={setSelectedDimension}
+                        isOpen={dimensionSelectorOpen}
+                        setToOpen={setDimensionSelectorOpen}
                 />
+                </div>
             </div>
-        </>
+            <Game
+                size={size}
+            />
+        </div>
     )
 }
 
